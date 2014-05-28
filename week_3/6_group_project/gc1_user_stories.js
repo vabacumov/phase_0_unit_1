a1fc4b0cfa0fe309512dd1582af2d08da047c138
Vladislav Abacumov <vabacumov@gmail.com>
2014-05-28 12:46:23 -0700

    creates user stories

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -1,3 +1,16 @@
 // My role in the group is:
-
+Person 1: User Stories
 // Here is my part of the challenge:
+1) As a store manager, I would like to have a program for my register
+that would allow me to take any given receipt and add up the 
+dollar amout of all purchases made on that receipt and give me the
+total sum.
+
+2) As a store manager, I would like to have a program for my register
+that would allow me to take a single receipt and find out what the
+average (mean) cost of purchases made on that receipt is.
+
+3) As a school teacher, I would like to have a program that would be
+able to intake the list of my students' grades (listed in alphabetical order) 
+and give me the grade that represents a point dividing the upper and lower
+halves of the class.
