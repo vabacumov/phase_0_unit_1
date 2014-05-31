02cdbc4ccdd2543b565176df1195c4c07f405136
Vladislav Abacumov <vabacumov@gmail.com>
2014-05-31 11:04:59 -0700

    Creates and idea for a mission

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,14 +1,16 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
-// 
+// *** Maze
+// The primary objective of the mission is to get out of the maze. The secondary objectives are:
+// 1) To grab the key in order to be able to exit the maze;
+// 2) To avoid running into an enemy. 
+// The player can move up, down, left or right. Minimum of five successful moves are necessary in order to complete the mission.
 
 // Pseudocode
-// 
-// 
-// 
+// Create an object "player"
+// Assign a few properties and values to it
+// Give it a couple of methods
 // 
 // 
 
