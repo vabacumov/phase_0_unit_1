a8f7f214ea681c2b4e86bc4d4d99f6453bc05aa3
Vladislav Abacumov <vabacumov@gmail.com>
2014-05-31 11:16:26 -0700

    Creates pseudocode

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,18 +1,22 @@
 // This is a solo challenge
 
 // Your mission description:
-*** Maze
+// Maze
 // The primary objective of the mission is to get out of the maze. The secondary objectives are:
 // 1) To grab the key in order to be able to exit the maze;
 // 2) To avoid running into an enemy. 
 // The player can move up, down, left or right. Minimum of five successful moves are necessary in order to complete the mission.
 
 // Pseudocode
-// Create an object "player"
-// Assign a few properties and values to it
-// Give it a couple of methods
-// 
-// 
+// Create an X, Y system of coordinates
+// Create "unit" object constructor
+// Include properties "name", "hp", "allegiance", "position"
+// Use the "unit" constructor to create objects "player" and "enemy"
+// Give object "player" methods "moveUp", "moveDown", "moveLeft", "moveRight"
+// Create object "key" with a property "position"
+// Give "player" property "hasKey"
+// Create a function inside the "player" called "checkKey"
+// Create a function inside the "player" called "checkObjective"
 
 // Initial Code
 
