1b162fec3a538be6744e78dfc18af71b438c780b
Vladislav Abacumov <vabacumov@gmail.com>
2014-06-01 11:15:14 -0700

    adds reflection

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -1,16 +1,25 @@
-// My role in the group is:
-Person 1: User Stories
-// Here is my part of the challenge:
-1) As a store manager, I would like to have a program for my register
-that would allow me to take any given receipt and add up the 
-dollar amout of all purchases made on that receipt and give me the
-total sum.
+/*  // My role in the group is: Person 1: User Stories
 
-2) As a store manager, I would like to have a program for my register
-that would allow me to take a single receipt and find out what the
-average (mean) cost of purchases made on that receipt is.
+// Here is my part of the challenge: 1) As a store manager, I would like to have a program for my
+register that would allow me to take any given receipt and add up the  dollar amout of all purchases
+made on that receipt and give me the total sum.
 
-3) As a school teacher, I would like to have a program that would be
-able to intake the list of my students' grades (listed in alphabetical order) 
-and give me the grade that represents a point dividing the upper and lower
-halves of the class.
+2) As a store manager, I would like to have a program for my register that would allow me to take a
+single receipt and find out what the average (mean) cost of purchases made on that receipt is.
+
+3) As a school teacher, I would like to have a program that would be able to intake the list of my
+students' grades (listed in alphabetical order)  and give me the grade that represents a point
+dividing the upper and lower halves of the class.
+
+// My Reflection This was our first group project, and I had to start it off, so I was definitely somewhat nervous.
+// Luckily, I was able to pair up with another person from our group and think about how to approach this assignment in
+// the best way possible. This gave me a lot of confidence as I understood the instructions and the whole process much
+// more clear. Once I knew what needed to be done, it wasnt' too hard to proceed from that point on. Also, I'd like to
+// say that despite the fact that as Person 1 in the group I didn't have to write actual code this time, I still took
+// time to think how I would attempt to write the required code and analyzed the final code given to me in details
+// before running it through the tests. As far as technical knowledge, one thing I picked up from this challenge was the
+// proper JS syntax on how to sort an array either numerically or alphabetically in an ascending or descending order.
+// This will certainly be helpful in the future. Overall, I feel like our group did a great job. Everyone has completed
+// their part of the assignment in time, our efforts were well coordinated and the communication was good. Finally, the
+// code passed all the tests successfully, which was great news! I really enjoyed working on this project.
+*/
